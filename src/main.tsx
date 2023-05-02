6636bb909f0632e3ec18890deb4f03eb6dbb92de
westy12dan <westy12dan@gmail.com>
2023-05-02 08:05:46 +0800

    start router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,43 @@
 import { render } from 'documentx'
 import { Todos } from './todos'
+import { hijackLinks } from './util/hijackLinks'
+import { createBrowserHistory } from 'history'
+import { ref } from './util'
+import { UserPage } from './user'
+
+const routes = {
+  '/': () => <h1>Home</h1>,
+  '/todos': () => <Todos />,
+  '/users/:id': () => <UserPage />,
+}
+
+const NotFound = () => <h1>Not Found</h1>
 
 const App = () => {
+  const el = ref()
+
+  const history = createBrowserHistory()
+  hijackLinks(history)
+
+  const getRouteElement = () => {
+    const path = history.location.pathname
+    const Route = routes[path as keyof typeof routes] || NotFound
+    return <Route />
+  }
+
+  history.listen(() => {
+    const route = getRouteElement()
+    el.target.replaceChildren(render(route))
+  })
+
   return (
     <div>
       <nav>
         <a href="/">Home</a>
         <a href="/todos">Todos</a>
+        <a href="/foo">Other</a>
       </nav>
-      <Todos />
+      <div ref={el}>{getRouteElement()}</div>
     </div>
   )
 }
---
+++ src/user.tsx
@@ -0,0 +1,58 @@
+export const UserPage = () => {
+  const user = {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  }
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <p>{user.website}</p>
+    </div>
+  )
+}
+
+type User = {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: string
+      lng: string
+    }
+  }
+  phone: string
+  website: string
+  company: {
+    name: string
+    catchPhrase: string
+    bs: string
+  }
+}
