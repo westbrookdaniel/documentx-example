9dc1fc81aed97e93c60937427841281b96cbca07
westy12dan <westy12dan@gmail.com>
2023-05-03 08:26:48 +0800

    add link

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import { Todos } from './todos'
 import { UserPage } from './user'
 
 export const router = createRouter({
-  '/': () => <h1>Home</h1>,
+  '/': () => (
+    <div>
+      <h1>Home</h1>
+      <p>Click on the links above to navigate</p>
+      <a href={`/users/${Math.floor(Math.random() * 10) + 1}`}>Random User</a>
+    </div>
+  ),
   '/todos': () => <Todos />,
   '/users/:id': () => <UserPage />,
   '404': () => <h1>Not Found</h1>,
