e1bb91cefca0a3dfb17821b887fdb29268e4539e
westy12dan <westy12dan@gmail.com>
2023-05-12 19:21:12 +0800

    fix user page

--- src/pages/users/:id.tsx
+++ src/pages/users/:id.tsx
@@ -18,7 +18,6 @@ const getUser = async (id: string) => {
 }
 
 export default async function UserPage() {
-  if (typeof document === 'undefined') return <div>Loading...</div>
   const id = router.params().id
   const user = await getUser(id)
   return (
