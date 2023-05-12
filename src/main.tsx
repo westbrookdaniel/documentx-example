c6b9f7f5679830235d54c7fe7c2f0fd3c5e83695
westy12dan <westy12dan@gmail.com>
2023-05-12 14:47:29 +0800

    yeet ssr

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './style.css'
 import { render } from 'documentx'
 import { ref, createRouter, lazy } from './util'
 
@@ -8,14 +9,10 @@ export const router = createRouter({
   '404': lazy(() => import('./pages/404')),
 })
 
-type InjectedProps = {
-  children?: JSX.Element
-}
-
-export default function App({ children }: InjectedProps) {
+export default function App() {
   const el = ref()
 
-  router.bind(el, {
+  const initalEl = router.bind(el, {
     loading: () => <h1>Loading...</h1>,
     error: (err) => <h1>Error: {err}</h1>,
   })
@@ -27,7 +24,7 @@ export default function App({ children }: InjectedProps) {
         <a href="/todos">Todos</a>
         <a href="/foo">Other</a>
       </nav>
-      <div ref={el}>{children}</div>
+      <div ref={el}>{initalEl}</div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import documentxserver from './plugin'
 
 export default defineConfig({
-  plugins: [documentxserver()],
+  build: {
+    minify: 'esbuild',
+  },
 })
