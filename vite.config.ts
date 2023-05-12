58af4cb1b92b507cee6863b55a753774df467d10
westy12dan <westy12dan@gmail.com>
2023-05-12 21:19:06 +0800

    move to ssr package

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,5 @@
-import path from 'node:path'
-import { Plugin, defineConfig } from 'vite'
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
-
-function documentxssr(): Plugin[] {
-  global.documentxssr = {
-    css: [],
-  }
-
-  return [
-    {
-      name: 'documentx-ssr-assets',
-      enforce: 'post',
-      apply: 'serve',
-      transform(_code, id, { ssr }) {
-        if (ssr && id.endsWith('.css')) {
-          const relativeId = path.relative(__dirname, id)
-          global.documentxssr.css.push('/' + relativeId)
-        }
-      },
-    },
-    {
-      name: 'documentx-ssr-build',
-      enforce: 'pre',
-      apply: 'build',
-      config(config, env) {
-        if (env.command === 'build' && config.build.ssr) {
-          return {
-            ...config,
-            build: {
-              ...config.build,
-              rollupOptions: {
-                input: {
-                  main: path.resolve(__dirname, 'src/main.tsx'),
-                },
-              },
-              emptyOutDir: false,
-            },
-          }
-        }
-        return config
-      },
-    },
-  ]
-}
+import { defineConfig } from 'vite'
+import documentxssr from 'documentx-ssr/plugin'
 
 export default defineConfig({
   plugins: [documentxssr()],
